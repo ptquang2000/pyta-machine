f8f92871e1c384a5fe0332310166178bbf1716d6
quang.phan <ptquang2000@gmail.com>
2024-03-18 21:37:44 +0700

    update clear script

--- scripts/clear.py
+++ scripts/clear.py
@@ -3,12 +3,13 @@ import shutil
 
 script_path = os.path.dirname(__file__)
 work_path = os.path.join(script_path, '..')
-tests_path = os.path.join(work_path, 'tests')
+test_path = os.path.join(work_path, 'test')
 
 day_dirs = [dir_name for dir_name in os.listdir(work_path) if 'day' == dir_name[0:3]]
 for day_dir in day_dirs:
     removed_dir = os.path.join(work_path, day_dir)
     shutil.rmtree(removed_dir)
 
-tests_init_file = os.path.join(tests_path, "__init__.py")
-os.remove(tests_init_file)
+test_init_file = os.path.join(test_path, "__init__.py")
+if os.path.exists(test_init_file):
+    os.remove(test_init_file)
