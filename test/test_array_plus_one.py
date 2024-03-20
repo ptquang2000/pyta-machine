a7c3570ae1ce2678d506f588f424dbc21c10a529
quang.phan <ptquang2000@gmail.com>
2024-03-20 07:09:50 +0700

    add test case plus one

--- test/test_array_plus_one.py
+++ test/test_array_plus_one.py
@@ -3,5 +3,5 @@ from test import plus_one
 
 class TestPlusOne(unittest.TestCase):
     def test_plus_one(self):
-        A = [1,2,9]
-        self.assertEqual(plus_one(A), [1,3,0])
+        self.assertEqual(plus_one([1,2,9]), [1,3,0])
+        self.assertEqual(plus_one([9,9,9]), [1,0,0,0])
