ea07f306cea8453ebe87c32a0740153bd8ff79cf
quang.phan <ptquang2000@gmail.com>
2024-03-27 07:40:04 +0700

    fix compile error

--- test/test_array_generate_primes.py
+++ test/test_array_generate_primes.py
@@ -3,4 +3,4 @@ from test import generate_primes
 
 class TestGeneratePrimes(unittest.TestCase):
     def test_generate_primes(self):
-        self.assertEqual(generate_primes(18, [2,3,5,7,11,13,17])
+        self.assertEqual(generate_primes(18), [2,3,5,7,11,13,17])
