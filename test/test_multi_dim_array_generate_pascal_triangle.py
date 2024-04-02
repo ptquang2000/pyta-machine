21ff497ff8e53f1dc9808e6849e73a76ffe58f31
quang.phan <ptquang2000@gmail.com>
2024-04-02 07:23:30 +0700

    add missing test

--- test/test_multi_dim_array_generate_pascal_triangle.py
+++ test/test_multi_dim_array_generate_pascal_triangle.py
@@ -3,10 +3,11 @@ from test import generate_pascal_triangle
 
 class TestGeneratePascalTriangle(unittest.TestCase):
     def test_generate_pascal_triangle(self):
-        resutl = [
+        result = [
             [1],
             [1,1],
             [1,2,1],
             [1,3,3,1],
             [1,4,6,4,1],
         ]
+        self.assertEqual(generate_pascal_triangle(5), result)
