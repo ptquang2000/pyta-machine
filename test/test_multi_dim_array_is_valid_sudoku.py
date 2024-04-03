75154b4f66c8e30b682e1933b3ad7148d3f8f114
quang.phan <ptquang2000@gmail.com>
2024-04-03 07:40:30 +0700

    update sudoku test

--- test/test_multi_dim_array_is_valid_sudoku.py
+++ test/test_multi_dim_array_is_valid_sudoku.py
@@ -14,7 +14,7 @@ class TestIsValidSudoku(unittest.TestCase):
             [0,0,0,4,1,9,0,0,5],
             [0,0,0,0,8,0,0,7,9],
         ]
-
+        self.assertEqual(is_valid_sudoku(partial_sudoku), False)
         completed_sudoku = [
             [5,3,4,6,7,8,9,1,2],
             [6,7,2,1,9,5,3,4,8],
@@ -27,3 +27,15 @@ class TestIsValidSudoku(unittest.TestCase):
             [3,4,5,2,8,6,1,7,9],
         ]
         self.assertEqual(is_valid_sudoku(completed_sudoku), True)
+        completed_sudoku = [
+            [5,3,4,6,7,8,9,1,2],
+            [6,7,2,1,9,5,3,4,8],
+            [1,9,8,3,4,2,5,6,7],
+            [8,5,9,7,6,1,4,2,3],
+            [4,2,6,8,5,3,7,9,1],
+            [7,1,3,9,2,4,8,5,6],
+            [9,6,1,5,3,7,2,1,4],
+            [2,8,7,4,1,9,6,3,5],
+            [3,4,5,2,8,6,1,7,9],
+        ]
+        self.assertEqual(is_valid_sudoku(completed_sudoku), False)
