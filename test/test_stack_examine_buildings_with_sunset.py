1ee07487571f1f854dd6cea3de254c9819b1a559
quang.phan <ptquang2000@gmail.com>
2024-05-15 07:37:26 +0700

    fix stack testcase

--- test/test_stack_examine_buildings_with_sunset.py
+++ test/test_stack_examine_buildings_with_sunset.py
@@ -3,5 +3,5 @@ from test import examine_buildings_with_sunset
 
 class TestExamineBuildingsWithSunset(unittest.TestCase):
     def test_examine_buildings_with_sunset(self):
-        self.assertEqual(examine_buildings_with_sunset([11, 12, 13, 14, 15]), [0, 1, 2, 3, 4])
-        self.assertEqual(examine_buildings_with_sunset([7, 4, 8, 2, 9]), [0, 2, 4])
+        self.assertEqual(examine_buildings_with_sunset([15,14,13, 12, 11]), [4,3,2,1,0])
+        self.assertEqual(examine_buildings_with_sunset([9,2,8,4,7]), [4,2,0])
